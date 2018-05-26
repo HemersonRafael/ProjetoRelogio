42a0f2f16619fd47f1a29db7a580156e9bff731b
HemersonRafael <rafaelpontes1995@gmail.com>
2018-05-26 18:05:44 -0300

    update registrador

--- relogio/registrador.vhd
+++ relogio/registrador.vhd
@@ -4,7 +4,7 @@ use ieee.std_logic_1164.all;
 entity registrador is
 	generic (N : integer := 8);
 	port (I : in std_logic_vector(N-1 downto 0);
-			enable, clock: in std_logic;
+			enable, clock, clear: in std_logic;
 			Q : out std_logic_vector (N-1 downto 0)
 	);
 end registrador;
@@ -13,9 +13,12 @@ architecture hardware of registrador is
 begin
 	process
 	begin
+	-- Espera até ocorrer um alteracao em clock
 		wait until clock'EVENT and clock ='1';
-		if enable = '1' then 
+		if enable = '1' and clear = '0' then 
 			Q <= I;
+		elsif clear = '1' then
+			Q <= "00000000";
 		end if;
 	end process;
 end hardware;
