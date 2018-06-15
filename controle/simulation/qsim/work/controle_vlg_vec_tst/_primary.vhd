6e938c2e6b3a5613045f8d77db64bb4a2643bc1b
HemersonRafael <rafaelpontes1995@gmail.com>
2018-06-15 15:20:34 -0300

    add projeto final

--- controle/simulation/qsim/work/controle_vlg_vec_tst/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity controle_vlg_vec_tst is
-end controle_vlg_vec_tst;
--- relogio/simulation/qsim/work/relogio_vlg_vec_tst/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity relogio_vlg_vec_tst is
-end relogio_vlg_vec_tst;
